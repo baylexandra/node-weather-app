3d46ea217121b2f9c15974d1854583018d53a254
Sasha Bayles <alexandrabayles@Alexandras-MBP.bayles.local>
2019-07-15 20:34:59 +1000

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log('Client-side js file is loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const forecastMessage = document.querySelector('#forecast')
+const locationMessage = document.querySelector('#location')
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    forecastMessage.textContent = ''
+    locationMessage.textContent = 'Loading...'
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                locationMessage.textContent = data.error
+            } else {
+                locationMessage.textContent = data.location
+                forecastMessage.textContent = data.forecast
+            }
+        })
+    })
+})
