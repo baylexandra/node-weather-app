896d3cbc6feb2b0efe32baf3dd901d72aedda964
Sasha Bayles <alexandrabayles@Alexandras-MacBook-Pro.local>
2019-07-16 13:49:05 +1000

    "Add temp"

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location')
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability*100}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability*100}% chance of rain. Min Temp: ${body.daily.data[0].temperatureLow} °C. Max Temp: ${body.daily.data[0].temperatureHigh} °C.`)
         }
     })
 }
